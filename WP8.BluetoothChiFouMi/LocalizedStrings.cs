98578704d9c823947df01a6fd1dd33db2cdd1c6e
Charly Attrait <c.attrait@iia-laval.fr>
2014-05-05 09:26:40 +0200

    First Commit

---
+++ WP8.BluetoothChiFouMi/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WP8.BluetoothChiFouMi.Resources;
+
+namespace WP8.BluetoothChiFouMi
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
